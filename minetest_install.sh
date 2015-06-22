c3414d04c4f033b30f16c861f5b6053ddd220f28
maximsecret <maximsecret12@gmail.com>
2015-06-22 12:06:35 +0600

    New system automatization

--- minetest_install.sh
+++ minetest_install.sh
@@ -1,25 +1,35 @@
 #Проверка на новую версию
+
+echo "Проверка на новую версию скриптов..."  &&
+git pull &&
+
 cd minetest &&
+echo "Проверка на новую версию minetest'a" &&
 git pull --rebase &&
 git submodule foreach git pull --rebase origin master &&
 cd .. &&
 cd minetest-game/games/minetest_game &&
+echo "Проверка на новую версию minetest_game" &&
 git pull --rebase &&
 git submodule foreach git pull --rebase origin master &&
 cd ../../../ &&
 
 #Сборка игры.
 #rm -R build &&
+echo "Начало компиляции" &&
 mkdir build &&
 cd build &&
 
 
 
 cmake ../minetest -DCMAKE_INSTALL_PREFIX="/" -DRUN_IN_PLACE=TRUE -DBUILD_SERVER=TRUE -DENABLE_GETTEXT=TRUE -DENABLE_GLES=TRUE -DCMAKE_CXX_FLAGS="-march=bdver1 -O3 -pipe -mno-movbe -mno-fma -mno-bmi -mno-tbm --param l1-cache-size=16 --param l1-cache-line-size=64 --param l2-cache-size=2048 -mtune=bdver1" -DCMAKE_C_FLAGS="-march=bdver1 -O3 -pipe -mno-movbe -mno-fma -mno-bmi -mno-tbm --param l1-cache-size=16 --param l1-cache-line-size=64 --param l2-cache-size=2048 -mtune=bdver1"  &&
+echo "Пожалуйста напишите флаги вашего процессора в minetest_install, либо в cmake-gui программе." &&
 cmake-gui ../minetest  &&
 make -j4 &&
 
 #УСТАНОВКА ИГРЫ
+echo "Установка в директорию minetest-game" &&
+
 make install DESTDIR=../minetest-game  &&
 cd .. &&
 
--- minetest_update_mods.sh
+++ minetest_update_mods.sh
@@ -1,73 +1,91 @@
 cd minetest-game/mods &&
 
+echo "Pipeworks" &&
 cd pipeworks &&
 git pull &&
 cd .. &&
 
+echo "Mesecons" &&
 cd mesecons &&
 git pull &&
 cd .. &&
 
+echo "Technic" &&
 cd technic &&
 git pull &&
 cd .. &&
 
+echo "Inventory Plus" &&
 cd inventory_plus &&
 git pull &&
 cd .. &&
 
+echo "zcg" &&
 cd zcg &&
 git pull &&
 cd .. &&
 
+echo "Weather" &&
 cd weather &&
 git pull &&
 cd .. &&
 
+echo "Nether" &&
 cd nether &&
 git pull &&
 cd .. &&
 
+echo "Hud" &&
 cd hud &&
 git pull &&
 cd .. &&
 
+echo "Hunger" &&
 cd hunger &&
 git pull &&
 cd .. &&
 
+echo "Carts" &&
 cd carts &&
 git pull &&
 cd .. &&
 
+echo "Creatures" &&
 cd creatures &&
 git pull &&
 cd .. &&
 
+echo "moreores" &&
 cd moreores &&
 git pull &&
 cd .. &&
 
+echo "3d armor" &&
 cd 3d_armor &&
 git pull &&
 cd .. &&
 
+echo "Intllib" &&
 cd intllib &&
 git pull &&
 cd .. &&
 
+echo "Factory" &&
 cd factory &&
 git pull &&
 cd .. &&
 
+echo "craft guide" &&
 cd craft_guide &&
 git pull &&
 cd .. &&
 
+echo "Torches" &&
 cd torches &&
 git pull &&
 cd .. &&
 
+echo "simple skins" &&
 cd simple_skins &&
 git pull &&
 cd .. &&
@@ -75,6 +93,7 @@ cd .. &&
 
 cd mymods_test &&
 cd extra_utilities &&
+echo "Extra Utilities" &&
 git pull &&
 cd .. &&
 
