19e5a53b2eec38dae5f65237631d15eabfc3fd26
maximsecret <maximsecret12@gmail.com>
2015-09-05 09:10:09 +0500

    Update minetest_prepare.sh

--- minetest_prepare.sh
+++ minetest_prepare.sh
@@ -1,6 +1,6 @@
 #Получение игры из репозитория.
 echo "Скачивание игры" &&
-git clone --depth 1 https://github.com/minetest/minetest.git 
+git clone --depth 1 https://github.com/minetest/minetest.git &&
 mkdir minetest-game &&
 
 echo "It's Ok."
